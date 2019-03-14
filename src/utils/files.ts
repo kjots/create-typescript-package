69c2f880237521cc5066b7f636c9cec4c946bedf
Karl Juri Ots <kjots@kjots.org>
2019-03-14 15:47:37 +1100

    Updated to use @kjots/observable-stream and @kjots/stream-observable.

--- src/utils/files.ts
+++ src/utils/files.ts
@@ -1,15 +1,16 @@
+import { observableStream } from '@kjots/observable-stream';
+import { streamObservable } from '@kjots/stream-observable';
+
 import { EMPTY, Observable } from 'rxjs';
 
 import vinylFs, { DestOptions, SrcOptions } from 'vinyl-fs';
 
 import File from 'vinyl';
 
-import { fromStream, toStream } from './streams';
-
 export function readFiles(globs: string | Array<string>, opt?: SrcOptions): Observable<File> {
-  return Array.isArray(globs) && globs.length === 0 ? EMPTY : fromStream(vinylFs.src(globs, opt));
+  return Array.isArray(globs) && globs.length === 0 ? EMPTY : streamObservable(vinylFs.src(globs, opt));
 }
 
 export function writeFiles(files: Observable<File>, folder: string, opt?: DestOptions): Observable<File> {
-  return fromStream(toStream(files).pipe(vinylFs.dest(folder, opt)));
+  return streamObservable(observableStream(files).pipe(vinylFs.dest(folder, opt)));
 }
